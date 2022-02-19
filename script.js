df9e706696d7a0819127b6258e6b8b9aac99a03d
Prajna1999 <gituprajna20@gmail.com>
2022-02-19 14:03:46 +0530

    Add: darkmode

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // selectors and selecting html element.
 const darkLight=document.querySelector(".dark-light");
-
+const body=document.querySelector("body")
 
 // eventlisteners.
 
 darkLight.addEventListener("click", ()=>{
     darkLight.classList.toggle("active");
+    body.classList.toggle("dark");
 })
